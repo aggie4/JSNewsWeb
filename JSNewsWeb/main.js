31d3b6d999b1c878498a2902ded63140c6471c69
KimSungJin <86825816+aggie4@users.noreply.github.com>
2022-08-08 20:08:40 +0900

    Add files via upload

--- JSNewsWeb/main.js
+++ JSNewsWeb/main.js
@@ -33,7 +33,7 @@ const getNews = async () => {
     }
     // header = new Headers({ })
     // fetch = 대기상태를 말한다
-    // async는 await 세트메뉴라고 생각하면 된다. "Respones" 응답이 왔다는 의미
+    // async는 await 세트메뉴라고 생각하면 된다. "Response" 응답이 왔다는 의미
     // 펑션 앞에다가 async, fetch앞에다가 await
     // json() = 왠만한 서버통신에 사용한다. 객체랑 같다. 다만, "텍스트 타입"인 객체이다. 시간이 걸려서 await 사용, 데이터 통신이 많으면 오류가 발생할 수 있다, 5초 정도 있다가 새로고침 하면 된다.
     // news = 우리가 볼 뉴스가 담겨져있다.
@@ -48,13 +48,16 @@ const getNews = async () => {
 */
 
 const getLatesNews = async () => {
+    page = 1;
     url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`);
     getNews();
 };
+// page = 1; 를 추가 , 새로운 페이지를 받을 때 1페이지로 가기 위해
 // API 정보 불러오기  , 자바스크립트에서 제공해주는 툴 , url 클레스 이용하기
 // new URL(`백틱 사용해서 링크 연결하기`) , 분석을 해준다
 
 const getNewsByTopic = async (event) => {
+    page = 1;
     //console.log('클릭 확인', event.target.textContent);
     let topic = event.target.textContent.toLowerCase();
     url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${topic}&page_size=10`);
@@ -70,6 +73,7 @@ const getNewsByTopic = async (event) => {
     위에 만든 header, response, data, news, render 를 복사하고 await가 있으니 함수 앞에 async를 붙혀서 오류가 나지 않게 만든다.
 */
 const getNewsByKeyword = async () => {
+    page = 1;
     // 1. 검색 키워드 읽어 오기
     // 2. 이거를 가지고 url에 검색 키워드 붙이기.
     // 3. header 준비
@@ -144,31 +148,42 @@ const pagention = () => {
     // 6. 첫페이지부터 마지막까지 프린트, 출력해주기.
     let pageGroup = Math.ceil(page / 5); //3번
     let last = pageGroup * 5; //4번
-    let first = last - 4; //5번
-    pagentionHTML = `<li class="page-item">
-    <a class="page-link" href="#" aria-label="Previous" onclick = "moveToPage(${page / 0})">
+
+    if (last > totalPage) {
+        // last 가, 마지막 그룹이 5개 이하라면..
+        last = totalPage;
+    }
+    let first = last - 4 <= 0 ? 1 : last - 4; //5번
+    // 페이지가 5개보다 작다면 숨기는 방법
+
+    if (first >= 6) {
+        pagentionHTML = `<li class="page-item">
+    <a class="page-link" href="#" aria-label="Previous" onclick = "moveToPage(1)">
     <span aria-hidden="true">&laquo;</span>
     </a></li>
-
     <li class="page-item">
     <a class="page-link" href="#" aria-label="Previous" onclick = "moveToPage(${page - 1})">
     <span aria-hidden="true">&lt;</span>
     </a></li>`;
-
+    }
+    // 페이지 숨기는 방법
     for (let i = first; i <= last; i++) {
         pagentionHTML += `<li class="page-item ${page == i ? 'active' : ''}"><a class="page-link" href="#" onclick="moveToPage(${i})">${i}</a></li>`;
     } // onclick함수추가해서 만들기. ${i}를 추가해서 어떤 페이지인지 알려준다. 페이지를 눌러도 아무런 변화가 없기 때문에
 
-    pagentionHTML += `
+    if (last < totalPage) {
+        pagentionHTML += `
     <li class="page-item">
     <a class="page-link" href="#" aria-label="Next" onclick = "moveToPage(${page + 1})">
     <span aria-hidden="true">&gt;</span>
     </a></li>
+
     <li class="page-item">
-    <a class="page-link" href="#" aria-label="Previous" onclick = "moveToPage(${page + 5})">
+    <a class="page-link" href="#" aria-label="Previous" onclick = "moveToPage(${totalPage})">
     <span aria-hidden="true">&raquo;</span>
     </a></li>`;
-    //마지막 페이지로 옮기는 것 안됨.. 뭐가 문제인 지 모르겠음
+        //마지막 페이지로 옮기는 것 안됨.. 뭐가 문제인 지 모르겠음 totalPage
+    }
     document.querySelector('.pagination').innerHTML = pagentionHTML;
 };
 const moveToPage = (pageNumber) => {
